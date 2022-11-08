6a0e5b890d3be76471d8e23a859d5da34ef54d48
Friezzie <aaronfriasbusiness@gmail.com>
2022-11-08 18:32:40 +0100

    Test

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x4541a55303de15cC659a5Cd19B3aC7dB097634C1";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-six-puce.vercel.app/api/";
+const METADATA_URL = "nft-collection-j5c22fs97-friezzie.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
